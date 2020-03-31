bbac19e4ad874ce5ea55eb09691f73149cbfffee
Parishrut <parishrutkushwaha@gmail.com>
2020-03-31 19:13:45 +0530

    Added colour buttons and fixed bugs

--- js.js
+++ js.js
@@ -2,6 +2,50 @@ let n=16;
 const cont=document.getElementById("container");
 let a="black";
 
+//code for blue colour
+let blue=document.querySelector('#blue');
+blue.addEventListener('click',function(e){
+  a="blue";
+})
+
+//code for red colour
+let red=document.querySelector('#red');
+red.addEventListener('click',function(e){
+  a="red";
+})
+
+//code for green colour
+let green=document.querySelector('#green');
+green.addEventListener('click',function(e){
+  a="green";
+})
+
+//code for black colour
+let black=document.querySelector('#black');
+black.addEventListener('click',function(e){
+  a="black";
+})
+
+//code for black colour
+let yellow=document.querySelector('#yellow');
+yellow.addEventListener('click',function(e){
+  a="yellow";
+})
+
+//code for random colour
+
+let rnd=document.querySelector('#random');
+rnd.addEventListener('click',function(e){
+  a='#'+Math.floor(Math.random()*16777215).toString(16);
+})
+
+//code for erase
+let erase=document.querySelector('#erase');
+erase.addEventListener('click',function(e){
+  a="#53A0AC";
+  document.body.style.cursor = "url('eraser.png',auto)";
+})
+
 //Function to make grid
 function grid(n){
   cont.style.gridTemplateColumns= `repeat(${n},1fr)`;
@@ -40,6 +84,7 @@ reset.addEventListener('click',function(e){
   grid(n);
 })
 
+//code for clear button
 let clr=document.querySelector('#clear');
 clr.addEventListener('click',function(e){
   clear();
