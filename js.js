5cc31c5288f19f6e8bc6c91bf767549ee37fe807
Parishrut <parishrutkushwaha@gmail.com>
2020-03-31 16:40:15 +0530

    Made small changes

--- js.js
+++ js.js
@@ -1,9 +1,15 @@
-let n=prompt("Enter grid size");
-const cont=document.getElementById('container');
+let n=5;
+const cont=document.getElementById("container");
 
-function grid(){
-  for(let i=0;i<n*n;i++){
-    let dom=document.createElement('div');
-    dom.classList.add('box');
-  }
-}
+function grid(n){
+  cont.style.gridTemplateColumns= `repeat(${n},1fr)`;
+  cont.style.gridTemplateRows= `repeat(${n},1fr)`;
+  for(let i=0;i<n;i++){
+    for(let j=0;j<n;j++){
+    let divs=document.createElement('div');
+    divs.className='box';
+    divs.style.backgroundColor="red";
+    cont.appendChild(divs);
+}}
+
+grid(n);
